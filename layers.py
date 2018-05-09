f9a49606d82160b73ecc3c78f87acd318292a88a
cotitan <cotitan@outlook.com>
2018-05-10 00:21:58 +0800

    working on decoder

--- layers.py
+++ layers.py
@@ -27,35 +27,47 @@ class SelectiveBiGRU(nn.Module):
         gru_out = gru_out * sGate
         return gru_out, self.hidden
 
+# NOTICE: encoder_hidden_dim * 2 = decoder_hidden_dim, since encoder is bi-directional
 
 class AttentionDecoder(nn.Module):
-    def __init__(self, vocab_size, emb_dim, hid_dim, encoder_hidden, test=False):
+    def __init__(self, vocab_size, emb_dim, hid_dim):
         super(AttentionDecoder, self).__init__()
         self.vocab_size = vocab_size
         self.emb_dim = emb_dim
         self.hid_dim = hid_dim
+        self.hidden = self.init_hidden()
+        self.encoder_hidden = encoder_hidden
+        self.encoder_out = encoder_out
 
         self.word_embeddings = nn.Embedding(vocab_size, emb_dim)
-        self.GRUdecoder = nn.GRU(emb_dim, hid_dim)
+        self.GRUdecoder = nn.GRU(emb_dim + hid_dim, hid_dim)
         self.linear = nn.Linear(hid_dim, vocab_size)
         self.softmax = nn.Softmax()
-        self.hidden = self.init_hidden()
         
-        self.attn_layer1 = nn.Linear(self.enc_hid_dim*2, 1) # since encoder is bi-directional
-        self.attn_layer2 = nn.Linear(self.dec_hid_dim, 1)
+        self.attn_layer1 = nn.Linear(self.hid_dim, 1) # since encoder is bi-directional
+        self.attn_layer2 = nn.Linear(self.hid_dim, 1)
 
 
     def init_hidden(self):
         return torch.zeros(1, 1, self.hid_dim)
 
-    def forward(self, sentence):
+    def forward(self, sentence, encoder_hidden, encoder_out, test=False):
         if test:
             # TODO: beam search
             pass
         else:
-            embeds = self.word_embeddings(sentence[0]) # that is <s>
-            self.hidden = encoder_hidden
-            attn_val = self.attn_layer1(encoder_hidden)
+            self.hidden = encoder_out
+            attn_1 = self.attn_layer1(encoder_hidden)
+            loss = 0
+            for i in range(len(sentence)-1):
+                embeds = self.word_embeddings(sentence[i])
+                attn_2 = self.attn_layer2(embeds)
+                attn_weights = self.softmax(attn_1 + attn_2) # 7*1 + 1; should be added for each elem in 7s
+                c_t = torch.sum(attn_weights * encoder_hidden, axis=0) #
+                dec_out, self.hidden = self.GRUdecoder(torch.cat(embeds, c_t), self.hidden)
+                probs = self.linear(self.hidden[0])
+                loss += -torch.log(probs[sentence[i+1]])
+            return loss
 
 def test():
     encoder = SelectiveBiGRU(10, 20, 30)
@@ -63,6 +75,7 @@ def test():
     # gru_out.size()=(len(x), batch_size, hid_dim*2)
     # hidden.size() = [2, batch_size, hid_dim*2]; here 2 means 2 layers
     gru_out, hidden = encoder(x)
+    decoder = 
     print(gru_out.shape, hidden.shape)
 
 if __name__ == '__main__':
