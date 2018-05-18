904b560658283865332f5e24428f068436e39b4e
cotitan <cotitan@outlook.com>
2018-05-18 18:09:48 +0800

    finishi beam search, but there are bugs, both in train.py and mytest.py

--- layers.py
+++ layers.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class Seq2SeqAttention(nn.Module):
-	def __init__(self, vocab_size, emb_dim, hid_dim, batch_size, device, beam_size=3, max_trg_len=25):
+	def __init__(self, vocab_size, emb_dim, hid_dim, batch_size, vocab, device, beam_size=3, max_trg_len=25):
 		super(Seq2SeqAttention, self).__init__()
 		self.vocab_size = vocab_size
 		self.emb_dim = emb_dim
@@ -12,6 +12,7 @@ class Seq2SeqAttention(nn.Module):
 		self.beam_size = beam_size
 		self.enc_hidden = None
 		self.dec_hidden = None
+		self.vocab = vocab
 		self.device = torch.device('cpu') if device is None else device
 		self.max_trg_len = max_trg_len
 
@@ -28,7 +29,7 @@ class Seq2SeqAttention(nn.Module):
 		self.linear_vocab = nn.Linear(hid_dim, vocab_size)
 		self.softmax = nn.Softmax(dim=-1)
 		self.attn_layer1 = nn.Linear(self.hid_dim, 1)
-		self.attn_layer2 = nn.Linear(self.emb_dim, 1)
+		self.attn_layer2 = nn.Linear(self.hid_dim, 1)
 
 	def init_hidden(self, batch_size):
 		return torch.zeros(2, batch_size, self.hid_dim // 2, device=self.device)
@@ -46,59 +47,69 @@ class Seq2SeqAttention(nn.Module):
 		states = states * sGate
 		return states, self.enc_hidden
 
-	def decode(self, enc_states, enc_hidden, test=False, sentence=None, vocab=None, st='<s>', ed='</s>'):
+	def decode(self, enc_states, enc_hidden, test=False, sentence=None, st='<s>', ed='</s>'):
 		batch_size = enc_states.shape[1]
 		if test:
 			# TODO: beam search
 			with torch.no_grad():
-				self.hidden = enc_hidden.view(1, -1, self.hid_dim)
+				self.dec_hidden = enc_hidden.view(1, -1, self.hid_dim)
 				attn_1 = self.attn_layer1(enc_states)
 				beam_size = self.beam_size
 
-				hidden_prev = torch.cat([self.hidden]*beam_size)
-				word_prev = torch.tensor([[vocab[st]]*beam_size]*batch_size, device=self.device)
+				h_prev = torch.cat([self.dec_hidden]*beam_size)  # shape=(beam_size, batch_size, hid_dim)
+				word_prev = torch.tensor([[self.vocab[st]]*beam_size]*batch_size, device=self.device)
 				acc_prob = torch.zeros(batch_size, beam_size, device=self.device)
 				summaries = torch.zeros(batch_size, beam_size, self.max_trg_len, device=self.device)
 
-				summaries[:, 0] = word_prev
+				summaries[:, :, 0] = word_prev
 
 				for i in range(self.max_trg_len - 1):
-					dec_hidden = torch.zeros(batch_size, beam_size, self.vocab_size, device=self.device)
+					dec_hidden = torch.zeros(batch_size, beam_size, self.hid_dim, device=self.device)
 
-					probs = torch.zeros(beam_size, self.vocab_size, device=self.device)
+					probs = torch.zeros(batch_size, beam_size, self.vocab_size, device=self.device)
 					for j in range(beam_size):
 						embeds = self.embedding_lookup(word_prev[:, j]).view(1, -1, self.emb_dim)
-						attn_2 = self.attn_layer2(embeds)
+						attn_2 = self.attn_layer2(h_prev[j])
 						# len*batch*dim + 1*batch*dim; should be added for each elem in
 						attn_weights = self.softmax((attn_1 + attn_2).view(batch_size, -1))
 						c_t = torch.sum(attn_weights.view(-1, batch_size, 1) * enc_states, dim=0).view(1, -1, self.hid_dim)  #
-						dec_states, dec_hidden[j] = self.GRUdecoder(torch.cat((embeds, c_t), dim=-1), hidden_prev[j])
-						probs[:, j] += acc_prob[:, j] - torch.log(self.softmax(self.linear_vocab(dec_states)))
+
+						dec_states, dec_hidden[:, j] = self.GRUdecoder(
+								torch.cat([embeds, c_t], dim=-1), h_prev[j].view(1, -1, self.hid_dim))
+						probs[:, j] = acc_prob[:, j].view(-1, 1) - torch.log(
+										self.softmax(self.linear_vocab(dec_states.view(-1, self.hid_dim))))
 
 					values, indices = torch.topk(probs.view(batch_size, -1), k=beam_size, largest=False)
 					acc_prob = values
 					summaries_cur = torch.tensor(summaries, device=self.device)
 					for j in range(beam_size):
-						hidden_prev[j] = dec_hidden[indices[j]//self.vocab_size]
+						for k in range(batch_size):
+							h_prev[j,k,:] = dec_hidden[k, indices[k,j]/self.vocab_size, :]
 						word_prev[:, j] = indices[:, j] % self.vocab_size
-						summaries_cur[:, j] = summaries[indices[:, j] // self.vocab_size]
-						summaries_cur[:, j][i+1] = word_prev[:, j]
+						for k in range(batch_size):
+							summaries_cur[k, j] = summaries[k, indices[k,j]/self.vocab_size, :]
+							summaries_cur[k, j, i+1] = word_prev[k, j]
+					summaries = summaries_cur
 				_, indices = torch.topk(acc_prob, k=1, largest=False)
-				return summaries[indices]
+
+				summary = torch.zeros(batch_size, self.max_trg_len, device=self.device)
+				for i in range(batch_size):
+					summary[i] = summaries[i, indices[i], :]
+				return summary
 
 		else:
-			self.hidden = enc_hidden.view(1, -1, self.hid_dim)
+			self.dec_hidden = enc_hidden.view(1, -1, self.hid_dim)
 			attn_1 = self.attn_layer1(enc_states)
 			loss = torch.zeros(1, device=self.device)
 			for i in range(self.max_trg_len - 1):
 				embeds = self.embedding_lookup(sentence[:, i]).view(1, -1, self.emb_dim)
-				attn_2 = self.attn_layer2(embeds)
+				attn_2 = self.attn_layer2(self.dec_hidden)
 				# len*batch*dim + 1*batch*dim; should be added for each elem in 7s
 				attn_weights = self.softmax((attn_1 + attn_2).view(len(sentence), -1))
 				c_t = torch.sum(attn_weights.view(-1, len(sentence), 1) * enc_states, dim=0).view(1, -1, self.hid_dim)  #
-				dec_states, self.dec_hidden = self.GRUdecoder(torch.cat((embeds, c_t), dim=-1), self.hidden)
+				dec_states, self.dec_hidden = self.GRUdecoder(torch.cat((embeds, c_t), dim=-1), self.dec_hidden)
 				probs = self.softmax(self.linear_vocab(dec_states))
 				for j in range(len(sentence)):
 					loss += -torch.log(probs[:, j, sentence[j, i + 1]])
-			return loss / len(sentence)
+			return loss / len(sentence) / self.max_trg_len
 
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description='Selective Encoding for Abstractive
 
 parser.add_argument('--gpu', type=int, default='1', help='GPU ID to use. For cpu, set -1 [default: -1]')
 parser.add_argument('--n_epochs', type=int, default=1, help='Number of epochs [default: 3]')
-parser.add_argument('--n_train', type=int, default=600000,
+parser.add_argument('--n_train', type=int, default=100000,
 					help='Number of training data (up to 3803957 in gigaword) [default: 3803957]')
 parser.add_argument('--n_valid', type=int, default=189651,
 					help='Number of validation data (up to 189651 in gigaword) [default: 189651])')
@@ -29,26 +29,30 @@ print('using device', device)
 
 def validate(validX, validY, model):
 	with torch.no_grad():
-		loss = torch.zeros(1, dtype=torch.float, device=device)
-		for idx, (batchX, batchY) in enumerate(zip(validX, validY)):
+		for _, (batchX, batchY) in enumerate(zip(validX, validY)):
 			batchX = torch.tensor(batchX, dtype=torch.long, device=device)
 			batchY = torch.tensor(batchY, dtype=torch.long, device=device)
-			loss += model(batchX, batchY)
-		loss /= idx
-		print('validation loss:', loss)
+			loss = model(batchX, batchY).cpu().numpy()
+			return loss
 
 
 def train(trainX, trainY, validX, validY, model, optimizer, scheduler, epochs=1):
+	steps = 0
 	for epoch in range(epochs):
 		for _, (batchX, batchY) in enumerate(zip(trainX, trainY)):
 			batchX = torch.tensor(batchX, dtype=torch.long, device=device)
 			batchY = torch.tensor(batchY, dtype=torch.long, device=device)
 			loss = model(batchX, batchY)
-			print(loss)
-
 			loss.backward(retain_graph=True)
 			optimizer.step()
 			scheduler.step()
+
+			steps += 1
+			if steps % 10 == 0:
+				train_loss = loss.cpu().detach().numpy()
+				valid_loss = validate(validX, validY, model)
+				# print('step %d, training loss = %f' % (steps, train_loss))
+				print('step %d, training loss = %f, validation loss = %f' % (steps, train_loss, valid_loss))
 
 		validate(validX, validY, model)
 		# torch.save(model, 'model_%d.pkl' % epoch)
@@ -78,15 +82,15 @@ def main():
 	validY = utils.getDataLoader(VALID_Y, max_len=25, n_data=N_VALID, batch_size=BATCH_SIZE)
 
 	vocab = json.load(open('data/vocab.json'))
-	model = Seq2SeqAttention(len(vocab), EMB_DIM, HID_DIM, BATCH_SIZE, device, max_trg_len=25).cuda(device)
+	model = Seq2SeqAttention(len(vocab), EMB_DIM, HID_DIM, BATCH_SIZE, vocab, device, max_trg_len=25).cuda(device)
 
 	if os.path.exists(model_dir) and len(os.listdir(model_dir)) > 0:
 		file = os.path.join(model_dir, os.listdir(model_dir)[-1])
 		model.load_state_dict(torch.load(file))
 		print('Load model parameters from %s' % file)
 
-	optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
-	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100000, gamma=0.3)
+	optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.3)
 
 	train(trainX, trainY, validX, validY, model, optimizer, scheduler, N_EPOCHS)
 
